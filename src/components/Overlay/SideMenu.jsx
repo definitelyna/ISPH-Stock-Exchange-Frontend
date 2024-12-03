ce6627abb2e7c55fd71b9c4670c2f4cd0ce3bbcf
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2024-12-03 08:19:30 +0700

    chane logo

--- src/components/Overlay/SideMenu.jsx
+++ src/components/Overlay/SideMenu.jsx
@@ -1,24 +1,24 @@
-import { styled } from "@mui/material/styles";
-import MuiDrawer, { drawerClasses } from "@mui/material/Drawer";
-import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
-import Stack from "@mui/material/Stack";
-import MenuContent from "./MenuContent";
-import OptionsMenu from "./OptionsMenu";
-import PropTypes from "prop-types";
-import CardMedia from "@mui/material/CardMedia";
-import UserCard from "./UserCard";
+import { styled } from '@mui/material/styles';
+import MuiDrawer, { drawerClasses } from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
+import MenuContent from './MenuContent';
+import OptionsMenu from './OptionsMenu';
+import PropTypes from 'prop-types';
+import CardMedia from '@mui/material/CardMedia';
+import UserCard from './UserCard';
 
 const drawerWidth = 240;
 
 const Drawer = styled(MuiDrawer)({
   width: drawerWidth,
   flexShrink: 0,
-  boxSizing: "border-box",
+  boxSizing: 'border-box',
   mt: 10,
   [`& .${drawerClasses.paper}`]: {
     width: drawerWidth,
-    boxSizing: "border-box",
+    boxSizing: 'border-box',
   },
 });
 
@@ -27,26 +27,26 @@ export default function SideMenu({ pageDict, currentPage, setCurrentPage }) {
     <Drawer
       variant="permanent"
       sx={{
-        display: { xs: "none", md: "block" },
+        display: { xs: 'none', md: 'block' },
         [`& .${drawerClasses.paper}`]: {
-          backgroundColor: "background.paper",
+          backgroundColor: 'background.paper',
         },
       }}
     >
       <Box
         sx={{
-          display: "flex",
-          mt: "calc(var(--template-frame-height, 0px) + 4px)",
+          display: 'flex',
+          mt: 'calc(var(--template-frame-height, 0px) + 4px)',
           p: 1.5,
-          height: 60
+          height: 60,
         }}
       >
         <Stack
           direction="row"
           sx={{
-            width: "100%",
+            width: '100%',
             gap: 1,
-            alignItems: "center",
+            alignItems: 'center',
           }}
         >
           <UserCard />
@@ -64,12 +64,12 @@ export default function SideMenu({ pageDict, currentPage, setCurrentPage }) {
 
       <CardMedia
         component="img"
-        image="../../public/isphLogo.png"
+        image="../../public/ISPH_LogoBlue.png" // TODO: Change Blue Logo to White on theme change
         sx={{
-          borderRadius: "50%",
-          width: "70%",
-          marginInline: "auto",
-          marginBottom: "13%",
+          borderRadius: '10%',
+          width: '70%',
+          marginInline: 'auto',
+          marginBottom: '13%',
         }}
       />
     </Drawer>
@@ -80,4 +80,3 @@ SideMenu.propTypes = {
   currentPage: PropTypes.string,
   setCurrentPage: PropTypes.func,
 };
-
