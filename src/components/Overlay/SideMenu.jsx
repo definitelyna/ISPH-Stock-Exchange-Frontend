95fdc847db092120f734038bbadd623142eeb66d
definitelyna <nguyentrongnamanh@gmail.com>
2024-12-13 08:50:39 +0700

    Changed useThemeDetector to useColorScheme

--- src/components/Overlay/SideMenu.jsx
+++ src/components/Overlay/SideMenu.jsx
@@ -1,44 +1,45 @@
-import { styled } from '@mui/material/styles';
-import MuiDrawer, { drawerClasses } from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Stack from '@mui/material/Stack';
-import MenuContent from './MenuContent';
-import OptionsMenu from './OptionsMenu';
-import PropTypes from 'prop-types';
-import CardMedia from '@mui/material/CardMedia';
-import UserCard from './UserCard';
-import useThemeDetector from '../../hooks/useThemeDetector';
+import { styled } from "@mui/material/styles";
+import { useColorScheme } from "@mui/material/styles";
+import MuiDrawer, { drawerClasses } from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import MenuContent from "./MenuContent";
+import OptionsMenu from "./OptionsMenu";
+import PropTypes from "prop-types";
+import CardMedia from "@mui/material/CardMedia";
+import UserCard from "./UserCard";
+import useThemeDetector from "../../hooks/useThemeDetector";
 
 const drawerWidth = 240;
 
 const Drawer = styled(MuiDrawer)({
   width: drawerWidth,
   flexShrink: 0,
-  boxSizing: 'border-box',
+  boxSizing: "border-box",
   mt: 10,
   [`& .${drawerClasses.paper}`]: {
     width: drawerWidth,
-    boxSizing: 'border-box',
+    boxSizing: "border-box",
   },
 });
 
 export default function SideMenu({ pageDict, currentPage, setCurrentPage }) {
-  const theme = useThemeDetector();
+  const { mode } = useColorScheme();
   return (
     <Drawer
       variant="permanent"
       sx={{
-        display: { xs: 'none', md: 'block' },
+        display: { xs: "none", md: "block" },
         [`& .${drawerClasses.paper}`]: {
-          backgroundColor: 'background.paper',
+          backgroundColor: "background.paper",
         },
       }}
     >
       <Box
         sx={{
-          display: 'flex',
-          mt: 'calc(var(--template-frame-height, 0px) + 4px)',
+          display: "flex",
+          mt: "calc(var(--template-frame-height, 0px) + 4px)",
           p: 1.5,
           height: 60,
         }}
@@ -46,9 +47,9 @@ export default function SideMenu({ pageDict, currentPage, setCurrentPage }) {
         <Stack
           direction="row"
           sx={{
-            width: '100%',
+            width: "100%",
             gap: 1,
-            alignItems: 'center',
+            alignItems: "center",
           }}
         >
           <UserCard />
@@ -66,12 +67,12 @@ export default function SideMenu({ pageDict, currentPage, setCurrentPage }) {
 
       <CardMedia
         component="img"
-        image={theme ? '/ISPH_LogoWhite.png' : '/ISPH_LogoBlue.png'}
+        image={mode == "dark" ? "/ISPH_LogoWhite.png" : "/ISPH_LogoBlue.png"}
         sx={{
-          borderRadius: '10%',
-          width: '70%',
-          marginInline: 'auto',
-          marginBottom: '13%',
+          borderRadius: "10%",
+          width: "70%",
+          marginInline: "auto",
+          marginBottom: "13%",
         }}
       />
     </Drawer>
