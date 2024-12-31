f3905a6c768825f85384e04623da36a1165e7b5c
definitelyna <nguyentrongnamanh@gmail.com>
2024-12-31 17:20:11 +0700

    Added gold silver bronze color to leaderboard

--- src/pages/dashboard/components/LeaderboardCard.jsx
+++ src/pages/dashboard/components/LeaderboardCard.jsx
@@ -2,6 +2,7 @@ import { Card, CardContent, Typography, Box } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { useColorScheme } from "@mui/material/styles";
 import { color } from "chart.js/helpers";
+import { getTextColor } from "../utils/getTextColor";
 
 const LeaderboardTestData = [
   {
@@ -26,19 +27,45 @@ const LeaderboardTestData = [
   },
 ];
 
+const getBackgroundColor = (index, colorTheme) => {
+  let returnValue = "";
+  if (index == 1) {
+    returnValue = "#FFD700";
+  } else if (index == 2) {
+    returnValue = "#D7D7D7";
+  } else if (index == 3) {
+    returnValue = "#CD7F32";
+  } else if (colorTheme == "dark") {
+    returnValue = "#FFFFFF";
+  } else {
+    returnValue = "#000000";
+  }
+
+  return returnValue;
+};
+
+
 const LeaderBoardCard = () => {
   const { mode } = useColorScheme();
-  const BOX_STYLE = {
-    display: "flex",
-    bgcolor: mode == "dark" ? "white" : "black",
-    width: "100%",
-    justifyContent: "center",
-    color: mode == "dark" ? "black" : "white",
+
+  const getBoxStyle = (index) => {
+    const bgcolor = getBackgroundColor(index, mode);
+
+    return {
+      display: "flex",
+      bgcolor: bgcolor,
+      width: "100%",
+      justifyContent: "center",
+      color: getTextColor(bgcolor),
+    };
   };
+
   return (
     <Card>
-      <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <Typography sx={{ fontWeight: "bold"}} variant="h6">
+      <CardContent
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Typography sx={{ fontWeight: "bold" }} variant="h6">
           Leaderboard
         </Typography>
         <Box sx={{ flexGrow: 1 }}>
@@ -59,9 +86,7 @@ const LeaderBoardCard = () => {
                   size={2}
                   sx={{ display: "flex", justifyContent: "center" }}
                 >
-                  <Box
-                    sx={BOX_STYLE}
-                  >
+                  <Box sx={getBoxStyle(index + 1)}>
                     <Typography fontWeight="bold">{index + 1}</Typography>
                   </Box>
                 </Grid>
@@ -70,9 +95,7 @@ const LeaderBoardCard = () => {
                   size={6}
                   sx={{ display: "flex", justifyContent: "center" }}
                 >
-                  <Box
-                    sx={BOX_STYLE}
-                  >
+                  <Box sx={getBoxStyle(index + 1)}>
                     <Typography fontWeight="bold">{data.name}</Typography>
                   </Box>
                 </Grid>
@@ -81,9 +104,7 @@ const LeaderBoardCard = () => {
                   size={4}
                   sx={{ display: "flex", justifyContent: "center" }}
                 >
-                  <Box
-                    sx={BOX_STYLE}
-                  >
+                  <Box sx={getBoxStyle(index + 1)}>
                     <Typography fontWeight="bold">{data.totalAsset}</Typography>
                   </Box>
                 </Grid>
