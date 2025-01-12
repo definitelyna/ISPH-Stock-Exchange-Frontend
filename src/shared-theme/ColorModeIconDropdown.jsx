8e010cfeda22bb28cd52a2f1e99eb018394ccf8f
definitelyna <nguyentrongnamanh@gmail.com>
2025-01-12 17:18:35 +0700

    Removed dropdown for light/dark mode changer

--- src/shared-theme/ColorModeIconDropdown.jsx
+++ src/shared-theme/ColorModeIconDropdown.jsx
@@ -1,26 +1,16 @@
-import * as React from 'react';
 import DarkModeIcon from '@mui/icons-material/DarkModeRounded';
 import LightModeIcon from '@mui/icons-material/LightModeRounded';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import { useColorScheme } from '@mui/material/styles';
 
 export default function ColorModeIconDropdown(props) {
   const { mode, systemMode, setMode } = useColorScheme();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+
+  const handleClick = () => {
+    setMode(mode == 'light' ? 'dark' : 'light');
   };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const handleMode = (targetMode) => () => {
-    setMode(targetMode);
-    handleClose();
-  };
+
   if (!mode) {
     return (
       <Box
@@ -42,43 +32,17 @@ export default function ColorModeIconDropdown(props) {
     light: <LightModeIcon />,
     dark: <DarkModeIcon />,
   }[resolvedMode];
+
   return (
-    <React.Fragment>
+    <>
       <IconButton
-        data-screenshot="toggle-mode"
         onClick={handleClick}
         disableRipple
         size="small"
-        aria-controls={open ? 'color-scheme-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
         {...props}
       >
         {icon}
       </IconButton>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        slotProps={{
-          paper: {
-            sx: {
-              my: '4px',
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        <MenuItem selected={mode === 'light'} onClick={handleMode('light')}>
-          Light
-        </MenuItem>
-        <MenuItem selected={mode === 'dark'} onClick={handleMode('dark')}>
-          Dark
-        </MenuItem>
-      </Menu>
-    </React.Fragment>
+    </>
   );
 }
